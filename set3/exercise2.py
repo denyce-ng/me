ac13960e6a7181b1b1b51fcc1195b4f05bcb4249
Denyce Ng <ngjenqin10@gmail.com>
2024-06-26 00:22:01 +1000

    jk

--- set3/exercise2.py
+++ set3/exercise2.py
@@ -6,7 +6,6 @@ is 🍟, or your guess is "pencil", or "seven"
 This will give you some intuition about how to make exercise 3 more robust.
 """
 
-
 import random
 
 
@@ -15,23 +14,35 @@ def exampleGuessingGame():
 
     This is an example guessing game. It'll test as an example too.
     """
-    print("\nWelcome to the guessing game!") #The string is printed in a new line
+    print("\nWelcome to the guessing game!")  # The string is printed in a new line
     print("A number between 0 and _ ?")
-    upperBound = input("Enter an upper bound: ") #prompt the user to input a number
-    print(f"OK then, a number between 0 and {upperBound} ?") #confirms upper bound entered by user 
-    upperBound = int(upperBound) #turns the input into an integer
+    upperBound = input("Enter an upper bound: ")  # prompt the user to input a number
+    print(
+        f"OK then, a number between 0 and {upperBound} ?"
+    )  # confirms upper bound entered by user
+    upperBound = int(upperBound)  # turns the input into an integer
 
-    actualNumber = random.randint(0, upperBound) #generates random number between 0 and upper bound
+    actualNumber = random.randint(
+        0, upperBound
+    )  # generates random number between 0 and upper bound
 
-    guessed = False #a variable is made which is used later in the loop
+    guessed = False  # a variable is made which is used later in the loop
 
-    while not guessed: #starts a loop which starts everytime guessed = False
-        guessedNumber = int(input("Guess a number: ")) #prompts user to guess a number then turns it into an input
-        print(f"You guessed {guessedNumber},") #confirms guessed Number
-        if guessedNumber == actualNumber: #checks if guessed number equals actual number
-            print(f"You got it!! It was {actualNumber}") #confirms it is the right number
-            guessed = True #sets guessed as True, which stops the loop
-        elif guessedNumber < actualNumber: #if the guessed number is less than the actual number, it will tell it is too small
+    while not guessed:  # starts a loop which starts everytime guessed = False
+        guessedNumber = int(
+            input("Guess a number: ")
+        )  # prompts user to guess a number then turns it into an input
+        print(f"You guessed {guessedNumber},")  # confirms guessed Number
+        if (
+            guessedNumber == actualNumber
+        ):  # checks if guessed number equals actual number
+            print(
+                f"You got it!! It was {actualNumber}"
+            )  # confirms it is the right number
+            guessed = True  # sets guessed as True, which stops the loop
+        elif (
+            guessedNumber < actualNumber
+        ):  # if the guessed number is less than the actual number, it will tell it is too small
             print("Too small, try again :'(")
         else:
             print("Too big, try again :'(")
